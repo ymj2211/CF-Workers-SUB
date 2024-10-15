ad0429483c01ce26685e296e8b95bed253c619b9
ymj2211 <126146174+ymj2211@users.noreply.github.com>
2024-10-16 05:45:18 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -1,7 +1,7 @@
 
 // 部署完成后在网址后面加上这个，获取自建节点和机场聚合节点，/?token=auto或/auto或
 
-let mytoken = '3ac2e9e3-57f2-40dd-99c5-18efb788588d'; //可以随便取，或者uuid生成，https://1024tools.com/uuid
+let mytoken = 'auto'; //可以随便取，或者uuid生成，https://1024tools.com/uuid
 let BotToken =''; //可以为空，或者@BotFather中输入/start，/newbot，并关注机器人
 let ChatID =''; //可以为空，或者@userinfobot中获取，/start
 let TG = 0; //小白勿动， 开发者专用，1 为推送所有的访问信息，0 为不推送订阅转换后端的访问信息与异常访问
@@ -12,21 +12,13 @@ let timestamp = 4102329600000;//2099-12-31
 
 //节点链接 + 订阅链接
 let MainData = `
-vmess://eyJ2IjoiMiIsInBzIjoiU0ctdm1lc3Mrd3MiLCJhZGQiOiJzZy55bWoyMjExLnh5eiIsInBvcnQiOiI0NDMiLCJpZCI6IjNhYzJlOWUzLTU3ZjItNDBkZC05OWM1LTE4ZWZiNzg4NTg4ZCIsImhvc3QiOiIiLCJwYXRoIjoiL3ltajIyMTEiLCJuZXQiOiJ3cyIsInNlY3VyaXR5IjoiYXV0byJ9
-vless://3ac2e9e3-57f2-40dd-99c5-18efb788588d@sg.ymj2211.xyz:38061?host=&path=%2Fymj2211&type=ws&encryption=none&security=tls&sni=sg.ymj2211.xyz#SG-vless+ws+tls
-vless://3ac2e9e3-57f2-40dd-99c5-18efb788588d@162.159.254.134:443?encryption=none&security=tls&sni=sz.ymj2211.xyz&type=ws&host=sz.ymj2211.xyz&path=%2F%3Fproxyip%3Dproxyip.aliyun.fxxk.dedyn.io#%F0%9F%9A%80CF%E4%BC%98%E9%80%89%E8%8A%82%E7%82%B9%201
-vless://3ac2e9e3-57f2-40dd-99c5-18efb788588d@104.19.79.238:443?encryption=none&security=tls&sni=sz.ymj2211.xyz&type=ws&host=sz.ymj2211.xyz&path=%2F%3Fproxyip%3Dproxyip.aliyun.fxxk.dedyn.io#%F0%9F%9A%80CF%E4%BC%98%E9%80%89%E8%8A%82%E7%82%B9%202
-vless://3ac2e9e3-57f2-40dd-99c5-18efb788588d@104.19.99.231:443?encryption=none&security=tls&sni=sz.ymj2211.xyz&type=ws&host=sz.ymj2211.xyz&path=%2F%3Fproxyip%3Dproxyip.aliyun.fxxk.dedyn.io#%F0%9F%9A%80CF%E4%BC%98%E9%80%89%E8%8A%82%E7%82%B9%203
-vless://3ac2e9e3-57f2-40dd-99c5-18efb788588d@104.20.14.130:443?encryption=none&security=tls&sni=sz.ymj2211.xyz&type=ws&host=sz.ymj2211.xyz&path=%2F%3Fproxyip%3Dproxyip.aliyun.fxxk.dedyn.io#%F0%9F%9A%80CF%E4%BC%98%E9%80%89%E8%8A%82%E7%82%B9%204
-vless://3ac2e9e3-57f2-40dd-99c5-18efb788588d@104.19.132.166:443?encryption=none&security=tls&sni=sz.ymj2211.xyz&type=ws&host=sz.ymj2211.xyz&path=%2F%3Fproxyip%3Dproxyip.aliyun.fxxk.dedyn.io#%F0%9F%9A%80CF%E4%BC%98%E9%80%89%E8%8A%82%E7%82%B9%205
-https://vless.durl.nyc.mn/sub?host=sz.ymj2211.xyz&uuid=3ac2e9e3-57f2-40dd-99c5-18efb788588d
-https://alvless.comorg.us.kg/sub?host=sz.ymj2211.xyz&uuid=3ac2e9e3-57f2-40dd-99c5-18efb788588d
-https://VLESS.fxxk.dedyn.io/sub?host=sz.ymj2211.xyz&uuid=3ac2e9e3-57f2-40dd-99c5-18efb788588d
+vless://b7a392e2-4ef0-4496-90bc-1c37bb234904@cf.090227.xyz:443?encryption=none&security=tls&sni=edgetunnel-2z2.pages.dev&fp=random&type=ws&host=edgetunnel-2z2.pages.dev&path=%2F%3Fed%3D2048#%E5%8A%A0%E5%85%A5%E6%88%91%E7%9A%84%E9%A2%91%E9%81%93t.me%2FCMLiussss%E8%A7%A3%E9%94%81%E6%9B%B4%E5%A4%9A%E4%BC%98%E9%80%89%E8%8A%82%E7%82%B9
+https://sub.xf.free.hr/auto
 `
 
 let urls = [];
 let subconverter = "SUBAPI.fxxk.dedyn.io"; //在线订阅转换后端，目前使用CM的订阅转换功能。支持自建psub 可自行搭建https://github.com/bulianglin/psub
-let subconfig = "https://raw.githubusercontent.com/ymj2211/ACL4SSR/refs/heads/main/Clash/config/ACL4SSR_Online_Full_MultiMode.ini"; //订阅配置文件
+let subconfig = "https://raw.githubusercontent.com/cmliu/ACL4SSR/main/Clash/config/ACL4SSR_Online_MultiCountry.ini"; //订阅配置文件
 let subProtocol = 'https';
 
 export default {
